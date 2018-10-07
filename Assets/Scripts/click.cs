794b38d3a5b5891f0758996dd1f66df5a024e180
Amil Patel <34750446+amil487@users.noreply.github.com>
2018-10-07 18:44:06 -0400

    swap

---
+++ Assets/Scripts/click.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class click : MonoBehaviour {
+    public Text counter;
+    int c = 0;
+    public GameObject special;
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+
+            c++;
+            counter.text = "Clicks: " + c.ToString();
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                MeshCollider collision = hit.collider as MeshCollider;
+                if (collision != null && collision.gameObject.tag == "trash")
+                    Destroy(collision.gameObject);
+
+                if (collision != null && collision.gameObject.tag == "turtle")
+                    collision.gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+                if (collision != null && collision.gameObject.tag == "special")
+                    special.SetActive(true);
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/walker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walker : MonoBehaviour {
+
+
+    bool mover = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.rotation.x == 0)
+        {
+            mover = true;
+        }
+
+        if (transform.rotation.z > 50 || transform.rotation.x > 50 || transform.rotation.y > 50)
+        {
+            mover = false;
+        }
+
+        if(mover)
+        {
+            transform.Translate(0, .01f, .01f);
+           // transform.Translate(0, Time.deltaTime, 0, Space.World);
+        }
+            
+
+    }
+}
